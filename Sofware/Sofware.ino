f545a51e56b3b9f903ddae1e14491c19128ecdf5
yushchyr <roman.yushchyk@gmail.com>
2018-01-08 22:07:36 -0600

    Initial commit

---
+++ Sofware/Sofware.ino
@@ -0,0 +1,13 @@
+// Constants
+  double Speed_Of_Light = 299792458; // Speed of light
+  
+// Variables
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
